bf2697e7eaeb7475f64fb272255cd80040d5cb4a
D'Amato <rocko83a@gmail.com>
2018-09-26 17:43:06 -0300

    add all the apps

---
+++ actionpoint/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ActionpointConfig(AppConfig):
+    name = 'actionpoint'
--- cloudsave/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CloudsaveConfig(AppConfig):
-    name = 'cloudsave'
